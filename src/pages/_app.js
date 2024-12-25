3e596953468728ba041e376fe88b7ce95f3b79fb
David Lebee <david.lebee@gluwa.com>
2024-12-25 10:10:16 -0500

    this should fix the vercel build?

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 import dynamic from 'next/dynamic';
 
 //const SatellitePosition2D = dynamic(() => import('./SatellitePosition2D'), { ssr: false });
-const SatellitePositionCesium = dynamic(() => import('./SatellitePositionCesium'), { ssr: false });
+const SatellitePositionCesium = dynamic(() => import('../components/SatellitePositionCesium'), { ssr: false });
 
 export default function App({ Component, pageProps }) {
   return <>
