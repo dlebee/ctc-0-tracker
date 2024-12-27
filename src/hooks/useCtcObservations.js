490ac2d59355a49d7f8e7a759c4ee77f562f217b
David Lebee <david.lebee@gluwa.com>
2024-12-27 13:06:03 -0500

    moved cache of observations on the server side, 10 min

--- src/hooks/useCtcObservations.js
+++ src/hooks/useCtcObservations.js
@@ -1,32 +1,6 @@
 import { fetchCtc0observationsFromSatNog } from '@/api/satnog';
 import { useState, useEffect } from 'react';
 
-const fetchOrCache = async function () {
-
-    const cacheKey = '__CACHED_OBSERVATIONS__';
-    const cacheExpKey = cacheKey + '_EXP';
-
-    // Check if the cache exists and is valid
-    const cachedData = localStorage.getItem(cacheKey);
-    const cachedTimestamp = localStorage.getItem(cacheExpKey);
-
-    if (cachedData && cachedTimestamp) {
-        const now = Date.now();
-        const expirationTime = parseInt(cachedTimestamp, 10);
-
-        // If the cache is still valid, return the cached data
-        if (now - expirationTime < 1 * 60 * 60 * 1000) { // 6 hours in milliseconds
-            return JSON.parse(cachedData);
-        }
-    }
-
-    // If no valid cache, fetch the content
-    const result = await fetchCtc0observationsFromSatNog();
-    localStorage.setItem(cacheKey, JSON.stringify(result));
-    localStorage.setItem(cacheExpKey, Date.now().toString());
-    return result;
-};
-
 
 const useCtcObservations = function () {
     const [data, setData] = useState([]);
@@ -38,7 +12,7 @@ const useCtcObservations = function () {
         let isMounted = true; // To avoid setting state if unmounted
 
         setLoading(true);
-        fetchOrCache()
+        fetchCtc0observationsFromSatNog()
             .then((data) => {
                 if (isMounted) {
                     setData(data);
--- src/pages/api/observations.js
+++ src/pages/api/observations.js
@@ -1,13 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import NodeCache from 'node-cache';
+const cache = new NodeCache({ stdTTL: 60*10 }); // 10 min TTL
+
 export default async function handler(req, res) {
+
+  const cache_key = '__OBSERVATIONS';
+
+  if (cache.has(cache_key)) {
+    return res.status(200).json(cache.get(cache_key));
+  }
+
   const apiBaseUrl = `https://network.satnogs.org`;
   const fullUrl = `${apiBaseUrl}/api/observations/?id=&status=good&satellite__norad_cat_id=98728`;
   const response = await fetch(fullUrl);
 
   if (response.status == 200) {
-    res.status(200).json(await response.json());
+    const json = await response.json();
+    cache.set(cache_key, json);
+    res.status(200).json(json);
   } else {
+    console.log('failed for reason', response.statusText, await response.text());
     res.status(response.status);
   }
 }
