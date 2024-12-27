6562df669979e1dc4b40772b5f637b632e320e47
David Lebee <david.lebee@gluwa.com>
2024-12-27 13:53:21 -0500

    returning all observations, regardless of status

--- src/pages/api/observations.js
+++ src/pages/api/observations.js
@@ -12,7 +12,8 @@ export default async function handler(req, res) {
   }
 
   const apiBaseUrl = `https://network.satnogs.org`;
-  const fullUrl = `${apiBaseUrl}/api/observations/?id=&status=good&satellite__norad_cat_id=98728`;
+  //const fullUrl = `${apiBaseUrl}/api/observations?status=good&satellite__norad_cat_id=98728`;
+  const fullUrl = `${apiBaseUrl}/api/observations?&satellite__norad_cat_id=98728`;
   const response = await fetch(fullUrl);
 
   if (response.status == 200) {
