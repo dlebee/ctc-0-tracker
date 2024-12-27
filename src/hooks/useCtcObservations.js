0b96c54294650ae4674653ea16c53bcd1358059d
David Lebee <david.lebee@gluwa.com>
2024-12-27 15:17:40 -0500

    refresh per 1 min

--- src/hooks/useCtcObservations.js
+++ src/hooks/useCtcObservations.js
@@ -7,26 +7,40 @@ const useCtcObservations = function () {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+
+
     useEffect(() => {
 
         let isMounted = true; // To avoid setting state if unmounted
 
-        setLoading(true);
-        fetchCtc0observationsFromSatNog()
-            .then((data) => {
-                if (isMounted) {
-                    setData(data);
-                    setLoading(false);
-                }
-            })
-            .catch(error => {
-                if (isMounted) {
-                    setError(error);
-                    setLoading(false);
-                }
-            });
+        const refreshObservations = function () {
+            setLoading(true);
+            fetchCtc0observationsFromSatNog()
+                .then((data) => {
+                    if (isMounted) {
+                        setData(data);
+                        setLoading(false);
+                    }
+                })
+                .catch(error => {
+                    if (isMounted) {
+                        setError(error);
+                        setLoading(false);
+                    }
+                });
+        }
 
-        return () => (isMounted = false); // Cleanup on unmount
+        refreshObservations();
+
+        const interval = setInterval(() => {
+            refreshObservations();
+        }, 60000); // every 1 min
+
+
+        return () => {
+            isMounted = false;
+            window.clearInterval(interval);
+         } // Cleanup on unmount
     }, []);
 
     return { data, loading, error };
