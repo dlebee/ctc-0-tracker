6b18fe5ef910ff11a1377d98459ceb0289c38001
David Lebee <david.lebee@gluwa.com>
2024-12-27 14:12:15 -0500

    order by date

--- src/pages/api/observations.js
+++ src/pages/api/observations.js
@@ -12,16 +12,32 @@ export default async function handler(req, res) {
   }
 
   const apiBaseUrl = `https://network.satnogs.org`;
-  //const fullUrl = `${apiBaseUrl}/api/observations?status=good&satellite__norad_cat_id=98728`;
-  const fullUrl = `${apiBaseUrl}/api/observations?&satellite__norad_cat_id=98728`;
-  const response = await fetch(fullUrl);
-
-  if (response.status == 200) {
-    const json = await response.json();
-    cache.set(cache_key, json);
-    res.status(200).json(json);
-  } else {
-    console.log('failed for reason', response.statusText, await response.text());
+  const goodUrl = `${apiBaseUrl}/api/observations?status=good&satellite__norad_cat_id=98728`;
+  const badUrl = `${apiBaseUrl}/api/observations?status=bad&satellite__norad_cat_id=98728`;
+  const responseGood = await fetch(goodUrl);
+  const responseBad = await fetch(badUrl);
+  
+  if (responseGood.status != 200) {
     res.status(response.status);
+    return;
   }
+
+  // get good data.
+  const goodData = await responseGood.json();
+
+  let result;
+  if (responseBad.status == 200) {
+    const badData = await responseBad.json();
+    result = goodData.concat(badData);
+    result.sort((a, b) => {
+      const aParsed = new Date(a.start);
+      const bParsed = new Date(b.start);
+      return bParsed - aParsed;
+    })
+  } else {
+    result = goodData;
+  }
+
+  cache.set(cache_key, result);
+  res.status(200).json(json);
 }
