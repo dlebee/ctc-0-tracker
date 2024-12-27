bd8b509e4d2f17c7f65c2e30ecc921fffb151ec0
David Lebee <david.lebee@gluwa.com>
2024-12-27 13:58:33 -0500

    cuter

--- src/components/observation.js
+++ src/components/observation.js
@@ -2,10 +2,9 @@ const Observation = ({ observation }) => {
 
     return (
         <div className="observation-card">
-            <h3 style={{ textAlign: 'center', marginBottom: '20px' }}>Latest Observation</h3>
+            <h3 style={{ textAlign: 'center', marginBottom: '5px' }}>Latest Observation</h3>
 
             <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-                <h4 style={{ margin: '0', fontSize: '16px' }}>Start Time</h4>
                 <p style={{ margin: '0', fontSize: '14px', color: '#ccc' }}>{observation.start}</p>
             </div>
 
